a273f3eab1b50386d2e911f90798bc84615369e5
Alen Rajher <alen.rajher@gmail.com>
2023-11-11 11:48:12 +0100

    added expense

---
+++ src/features/transactions/components/AddExpense.tsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+const AddExpense = () => {
+	// amount, category dropdown (Needs, Wants, Savings), description?
+
+	return <div>AddExpense</div>;
+};
+
+export default AddExpense;
--- src/features/transactions/components/AddTransaction.tsx
+++ src/features/transactions/components/AddTransaction.tsx
@@ -1,11 +1,12 @@
 import React from "react";
 import AddIncome from "./AddIncome";
+import AddExpense from "./AddExpense";
 
 const AddTransaction = ({ type }: { type: string }) => {
 	if (type === "income") {
 		return <AddIncome />;
 	} else if (type === "expense") {
-		return "expense";
+		return <AddExpense />;
 	}
 
 	return null;
