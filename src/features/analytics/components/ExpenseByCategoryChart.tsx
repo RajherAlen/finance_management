9f79b96618a6c925c7ad252ba731714d65ba5f2e
Alen Rajher <alen.rajher@gmail.com>
2024-01-05 09:43:20 +0100

    added new bubble chart

--- src/features/analytics/components/ExpenseByCategoryChart.tsx
+++ src/features/analytics/components/ExpenseByCategoryChart.tsx
@@ -2,6 +2,7 @@
 
 import React from 'react';
 
+import BubbleChart from '@weknow/react-bubble-chart-d3';
 import { useAppSelector } from 'src/store/hooks';
 
 const ExpenseByCategoryChart = () => {
@@ -9,7 +10,40 @@ const ExpenseByCategoryChart = () => {
 
     if (!transactions) return null;
 
-    return <div>BUBBLE CHART</div>;
+    return (
+        <div>
+            <p className="mb-2 text-xl font-semibold">Expenses by category</p>
+            <BubbleChart
+                width={600}
+                height={600}
+                padding={0}
+                graph={{
+                    zoom: 0.8,
+                    offsetX: -0.05,
+                    offsetY: -0.01,
+                }}
+                showLegend={false}
+                fontFamily="Arial"
+                valueFont={{
+                    family: 'Arial',
+                    size: 16,
+                    color: '#fff',
+                }}
+                labelFont={{
+                    family: 'Arial',
+                    size: 11,
+                    color: '#fff',
+                    weight: 'normal'
+                }}
+                data={transactions.map((transaction) => {
+                    return {
+                        label: transaction.description,
+                        value: transaction.amount,
+                    };
+                })}
+            />
+        </div>
+    );
 };
 
 export default ExpenseByCategoryChart;
