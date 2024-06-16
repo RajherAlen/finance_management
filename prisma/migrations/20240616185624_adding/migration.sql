a9e11e3298a113147de4671039b4aa908b3c822e
Alen Rajher <alen.rajher@gmail.com>
2024-06-16 20:59:00 +0200

    adding loans table

---
+++ prisma/migrations/20240616185624_adding/migration.sql
@@ -0,0 +1,19 @@
+-- CreateTable
+CREATE TABLE `loans` (
+    `id` INTEGER NOT NULL AUTO_INCREMENT,
+    `totalAmount` DOUBLE NOT NULL,
+    `paid` DOUBLE NOT NULL DEFAULT 0,
+    `interestRate` DOUBLE NOT NULL,
+    `startDate` DATETIME(3) NOT NULL DEFAULT CURRENT_TIMESTAMP(3),
+    `endDate` DATETIME(3) NOT NULL,
+    `status` ENUM('PENDING', 'APPROVED', 'REJECTED', 'COMPLETED') NOT NULL DEFAULT 'PENDING',
+    `description` VARCHAR(191) NULL,
+    `userId` INTEGER NOT NULL,
+    `createdAt` DATETIME(3) NOT NULL DEFAULT CURRENT_TIMESTAMP(3),
+    `updatedAt` DATETIME(3) NOT NULL,
+
+    PRIMARY KEY (`id`)
+) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+
+-- AddForeignKey
+ALTER TABLE `loans` ADD CONSTRAINT `loans_userId_fkey` FOREIGN KEY (`userId`) REFERENCES `User`(`id`) ON DELETE RESTRICT ON UPDATE CASCADE;
