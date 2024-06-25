54854ef179420ab8586ea7b159f0ba8f594b82fd
Alen Rajher <arajher@mono.software>
2024-06-25 19:18:21 +0200

    loan check

--- src/features/loans/utils/addLoanToTransaction.ts
+++ src/features/loans/utils/addLoanToTransaction.ts
@@ -1,18 +1,13 @@
 import React from 'react';
-
 import { Transaction } from 'src/features/transactions/model/transactionModel';
-
 import { formatDate } from 'src/lib/utils/formatDate';
-
 import { Loan } from '../model/loanModel';
-
 interface DataProps {
     transactionData: Transaction[];
     loansData: Loan[];
     addTransaction: (transaction: TransactionProps) => void;
     userId: number;
 }
-
 interface TransactionProps {
     amount: number;
     description: string;
@@ -22,30 +17,37 @@ interface TransactionProps {
     date: Date;
     recurring: boolean;
 }
-
 const addLoanToTransaction = ({ transactionData, loansData, addTransaction, userId }: DataProps) => {
     const currentYear = formatDate({ date: new Date(), format: 'YYYY' });
     const currentMonth = formatDate({ date: new Date(), format: 'MM' });
-
+    const currentDay = formatDate({ date: new Date(), format: 'DD' });
     // Filter transactions that are loans
     const filteredTransactions = transactionData?.filter((transaction: Transaction) => transaction.category === 'loan');
-
     // Iterate over loans
     loansData?.forEach((loan: Loan) => {
         const loanYear = String(loan.endDate).split('-')[0];
         const loanDay = String(loan.endDate).split('-')[2];
-
-        console.log(filteredTransactions);
         // Check if the loan should be added for the current month and year
-        if (currentYear <= loanYear) {
+        if (currentYear <= loanYear && currentDay >= loanDay) {
+            if (filteredTransactions?.length === 0) {
+                addTransaction({
+                    amount: loan.instalmentAmount,
+                    // TODO: change this description/name
+                    description: `loan-${loan.name}`,
+                    category: 'loan',
+                    type: 'expense',
+                    userId: userId,
+                    date: new Date(`${currentYear}-${currentMonth}-${loanDay}`),
+                    recurring: true,
+                });
+            }
+            // Check if transaction already exists for this loan in current month
             if (filteredTransactions && filteredTransactions.length > 0) {
-                // Check if transaction already exists for this loan in current month
                 const transactionExists = filteredTransactions.find(
                     (transaction: Transaction) =>
                         transaction.description === `loan-${loan.name}` &&
                         String(transaction.date).split('T')[0] === `${currentYear}-${currentMonth}-${loanDay}`
                 );
-
                 // If transaction does not exist, add it
                 if (!transactionExists) {
                     addTransaction({
@@ -59,20 +61,8 @@ const addLoanToTransaction = ({ transactionData, loansData, addTransaction, user
                         recurring: true,
                     });
                 }
-            } else {
-                addTransaction({
-                    amount: loan.instalmentAmount,
-                    // TODO: change this description/name
-                    description: `loan-${loan.name}`,
-                    category: 'loan',
-                    type: 'expense',
-                    userId: userId,
-                    date: new Date(`${currentYear}-${currentMonth}-${loanDay}`),
-                    recurring: true,
-                });
             }
         }
     });
 };
-
 export default addLoanToTransaction;
