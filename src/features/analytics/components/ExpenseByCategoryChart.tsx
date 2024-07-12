5b556e732835e2bd8940fcebb628896d87cd05b9
Alen Rajher <arajher@mono.software>
2024-07-12 08:49:17 +0200

    removed bubble chart

--- src/features/analytics/components/ExpenseByCategoryChart.tsx
+++ src/features/analytics/components/ExpenseByCategoryChart.tsx
@@ -9,15 +9,13 @@ import { useAppSelector } from 'src/store/hooks';
 
 const ExpenseByCategoryChart = () => {
     const { transactions } = useAppSelector((state) => state.transactionStore);
-
+    return null;
     if (transactions.length === 0) return <EmptyState title="No Transactions Yet" />;
 
     return (
         <div>
             <p className="mb-5 text-xl font-semibold">Expenses by category</p>
-            <div className="flex justify-center p-5">
-                <CustomBubbleChart data={transactions} />
-            </div>
+            <div className="flex justify-center p-5">{/* <CustomBubbleChart data={transactions} /> */}</div>
         </div>
     );
 };
