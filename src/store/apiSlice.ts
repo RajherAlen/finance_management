5da8f984d12747fe70581c5cc71d7224b0141e99
Alen Rajher <alen.rajher@gmail.com>
2026-02-23 21:12:15 +0100

    api changes (CORS)

--- src/store/apiSlice.ts
+++ src/store/apiSlice.ts
@@ -2,10 +2,8 @@ import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react';
 
 import { RootState } from './store';
 
-const BASE_URL = process.env.NODE_ENV === 'production' ? 'https://finance-management-steel.vercel.app/' : 'http://localhost:3000';
-
 const baseQuery = fetchBaseQuery({
-    baseUrl: `${BASE_URL}/api`,
+    baseUrl: '/api',
     prepareHeaders(headers, { getState }) {
         const token = (getState() as RootState).authStore.userToken;
 
